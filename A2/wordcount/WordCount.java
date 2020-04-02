12a57458d5ca544a1466dbb5ff4c7e4096b125aa
Daniel Ågstrand <daniel.agstrand.5971@student.uu.se>
2020-04-02 21:10:30 +0000

    Modified Wordcount executed

--- A2/wordcount/WordCount.java
+++ A2/wordcount/WordCount.java
@@ -13,13 +13,13 @@ public class WordCount {
   public static class TokenizerMapper
        extends Mapper<Object, Text, Text, IntWritable>{
     private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text("COUNT");
+    private Text word = new Text();
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
       StringTokenizer itr = new StringTokenizer(value.toString());
       while (itr.hasMoreTokens()) {
         String token = itr.nextToken();
-        if (token.startsWith("a-z")) {
+        if(Character.isLetter(token.charAt(0))) {
           word.set(String.valueOf(token.charAt(0)));
           context.write(word, one);
         }
