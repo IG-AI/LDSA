b2922c6d89211747f27222a22f00eb67385c608a
Daniel Ågstrand <daniel.agstrand.5971@student.uu.se>
2020-04-03 20:34:08 +0000

    Twitter analysis done

--- A2/tweet_analysis/mapper.py
+++ A2/tweet_analysis/mapper.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 """mapper.py"""
 
-import sys
+import sys, re, json;
+
+twitter_data = []
+for line in sys.stdin:
+    if not line.isspace():
+        data = json.loads(line)
+        twitter_data.append(data["text"])
 
 # input comes from STDIN (standard input)
-for line in sys.stdin:
+for line in twitter_data:
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
     words = line.split()
-    # increase counters
+    # Creates a regex that looks for everything that's not a swedish letter.
+    regex = re.compile('[^a-öA-Ö]')
+    # The sets a list with pronouns that should be search for. 
+    pronouns_list = ["han", "hon", "denna", "det", "denne", "den", "hen"]
     for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print("%s\t%s" % (word, 1))
+        # Splits the word into a list of words on the occurrence of the regex
+        words_split = re.split(regex, word)
+        # Loops through the split word list and looks for lower case occurrence of in the pronouns list
+        # If it finds a occurrence then it writes the result togheter with the amount of occurrence to STDOUT
+        for split_word in words_split:
+            if split_word.lower() in pronouns_list: 
+                print("%s\t%s" % (split_word.lower(), 1))
+                
+                
